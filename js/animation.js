e624704658e7c4d5b87597022d9c2841f23d486c
Thay <pattasiwa@gmail.com>
2021-05-11 19:30:33 -0400

    about page responsive - mobile first!

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,6 @@
 const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } })
 
-timeline.to('.text', { x: '0%', duration: 1.2, stagger: 0.2 })
+timeline.to('.text', { x: '0%', duration: 1, stagger: 0.5 })
 timeline.to('.slider', { y: "-100%", duration: 1.7, delay: 0.6 })
 timeline.to('.intro', { x: "-100%", duration: 1 }, "-=.9")
 timeline.fromTo('header', { opacity: 0 }, { opacity: 1, duration: 1.5 })
