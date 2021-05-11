f559cc5126417ee8ea4a581e0d87c54df37039a2
Thay <pattasiwa@gmail.com>
2021-05-11 15:44:58 -0400

    more animation tinkering

--- js/animation.js
+++ js/animation.js
@@ -1,5 +1,7 @@
 const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } })
 
-timeline.to('.text', { x: '0%', duration: 1, stagger: 0.2 })
+timeline.to('.text', { x: '0%', duration: 1.2, stagger: 0.2 })
 timeline.to('.slider', { y: "-100%", duration: 1.7, delay: 0.6 })
-timeline.to('.intro', { y: "-100%", duration: 1 }, "-=.9")
+timeline.to('.intro', { x: "-100%", duration: 1 }, "-=.9")
+timeline.fromTo('header', { opacity: 0 }, { opacity: 1, duration: 1 })
+timeline.fromTo('.title-text', { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1")
