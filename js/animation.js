a8c4321c0f5240ee75ff192f0516c731d1c722b8
Thay <pattasiwa@gmail.com>
2021-05-12 17:39:09 -0400

    experience section complete

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } })
 
-timeline.to('.text', { x: '0%', duration: .1, stagger: 0.5 })
-timeline.to('.slider', { y: "-100%", duration: .17, delay: 0.6 })
-timeline.to('.intro', { x: "-100%", duration: .1 }, "-=.9")
+timeline.to('.text', { x: '0%', duration: .7, stagger: 0.5 })
+timeline.to('.slider', { y: "-100%", duration: 1.3, delay: 0.3 })
+timeline.to('.intro', { x: "-100%", duration: .7 }, "-=.7")
 timeline.fromTo('header', { opacity: 0 }, { opacity: 1, duration: .15 })
-timeline.fromTo('.title-text', { opacity: 0 }, { opacity: 1, duration: .1 }, "-=1.5")
+timeline.fromTo('.title-text', { opacity: 0 }, { opacity: 1, duration: .7 }, "-=1.3")
