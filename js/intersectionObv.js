b424d77e876c5c748fa39929e9059248b7c3235b
Thay <pattasiwa@gmail.com>
2021-05-12 08:01:07 -0400

    added in smooth scroll and fade in animation in about

---
+++ js/intersectionObv.js
@@ -0,0 +1,22 @@
+const faders = document.querySelectorAll('.fade-in')
+
+
+const appearOptions = {
+    threshold: .7,
+}
+
+
+const appearOnScroll = new IntersectionObserver((entries, appearOnScroll) => {
+    entries.forEach(entry => {
+        if (!entry.isIntersecting) {
+            return
+        } else {
+            entry.target.classList.add('appear')
+            appearOnScroll.unobserve(entry.target)
+        }
+    })
+}, appearOptions)
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader)
+})
