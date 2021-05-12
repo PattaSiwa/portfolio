e023e28dcbfbda7d4212acd17985ac52ecdbe7c4
Thay <pattasiwa@gmail.com>
2021-05-12 18:09:38 -0400

    education added

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,7 @@
 const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } })
 
-timeline.to('.text', { x: '0%', duration: .7, stagger: 0.5 })
-timeline.to('.slider', { y: "-100%", duration: 1.3, delay: 0.3 })
-timeline.to('.intro', { x: "-100%", duration: .7 }, "-=.7")
-timeline.fromTo('header', { opacity: 0 }, { opacity: 1, duration: .15 })
-timeline.fromTo('.title-text', { opacity: 0 }, { opacity: 1, duration: .7 }, "-=1.3")
+timeline.to('.text', { x: '0%', duration: .9, stagger: 0.5 })
+timeline.to('.slider', { y: "-100%", duration: 1.4, delay: 0.3 })
+timeline.to('.intro', { x: "-100%", duration: .9 }, "-=.9")
+timeline.fromTo('header', { opacity: 0 }, { opacity: 1, duration: 1.5 })
+timeline.fromTo('.title-text', { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1.5")
