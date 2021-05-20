c3f6b1875f3e635698d9d914c376904f7613f670
Thay <pattasiwa@gmail.com>
2021-05-20 06:49:16 -0400

    changed beginning animation and font size for big screen

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,6 @@
 const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } })
 
-timeline.to('.text', { x: '0%', duration: .9, delay: 1, stagger: .7 })
+timeline.to('.text', { x: '0%', duration: 1.5, delay: 1, stagger: 1 })
 timeline.to('.slider', { y: "-100%", duration: 1.4, delay: 0.3 })
 timeline.to('.intro', { x: "-100%", duration: .9 }, "-=.9")
 timeline.fromTo('header', { opacity: 0 }, { opacity: 1, duration: 1.5 })
