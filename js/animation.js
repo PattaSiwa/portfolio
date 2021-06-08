39e4d1db4d8ef87f11b29b5ec6a7441b9b18849c
Thay <pattasiwa@gmail.com>
2021-06-08 17:03:09 -0400

    added prettier for formatting

--- js/animation.js
+++ js/animation.js
@@ -1,7 +1,12 @@
-const timeline = gsap.timeline({ defaults: { ease: 'power1.out' } })
+const timeline = gsap.timeline({ defaults: { ease: "power1.out" } });
 
-timeline.to('.text', { x: '0%', duration: 1.5, delay: 1, stagger: 1 })
-timeline.to('.slider', { y: "-100%", duration: 1.4, delay: 0.3 })
-timeline.to('.intro', { x: "-100%", duration: .9 }, "-=.9")
-timeline.fromTo('header', { opacity: 0 }, { opacity: 1, duration: 1.5 })
-timeline.fromTo('.title-text', { opacity: 0 }, { opacity: 1, duration: 1 }, "-=1.5")
+timeline.to(".text", { x: "0%", duration: 1.5, delay: 1, stagger: 1 });
+timeline.to(".slider", { y: "-100%", duration: 1.4, delay: 0.3 });
+timeline.to(".intro", { x: "-100%", duration: 0.9 }, "-=.9");
+timeline.fromTo("header", { opacity: 0 }, { opacity: 1, duration: 1.5 });
+timeline.fromTo(
+  ".title-text",
+  { opacity: 0 },
+  { opacity: 1, duration: 1 },
+  "-=1.5"
+);
